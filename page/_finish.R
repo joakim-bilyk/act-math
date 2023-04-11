a9aec5cd1c1203147804a51dfee3e46c37bf8c37
joakim-bilyk <joakim.bilyk@outlook.com>
2023-04-11 19:23:35 +0200

    update

--- page/_finish.R
+++ page/_finish.R
@@ -28,40 +28,40 @@ for (f in formats) {
   file.remove(files)
 }
 
+#Get statics
+unlink("joakim-bilyk.github.io/docs/static",recursive = TRUE)
+dir.create("joakim-bilyk.github.io/docs/static")
+files <- list.files(path = "_assets/static",include.dirs = T, full.names = T)
+file.copy(files,"joakim-bilyk.github.io/docs/static",recursive = TRUE)
+
 # Render theory book
-files <- list.files(path = "_assets/theory/docs",include.dirs = T, full.names = T)
-file.remove(files)
+unlink("_assets/theory/docs",recursive = TRUE)
+dir.create("_assets/theory/docs")
 rmarkdown::render_site("_assets/theory",encoding = 'ISO8859-1')
 unlink("joakim-bilyk.github.io/docs/books",recursive = TRUE)
 dir.create("joakim-bilyk.github.io/docs/books")
 dir.create("joakim-bilyk.github.io/docs/books/theory")
 files <- list.files(path = "_assets/theory/docs",include.dirs = T, full.names = T)
 file.copy(files,"joakim-bilyk.github.io/docs/books/theory",recursive = TRUE)
-file.remove("joakim-bilyk.github.io/docs/pdf/render/theory.pdf")
-file.copy("joakim-bilyk.github.io/docs/books/theory/theory.pdf","joakim-bilyk.github.io/docs/pdf/render/theory.pdf")
+file.remove("joakim-bilyk.github.io/docs/static/theory.pdf")
+file.copy("joakim-bilyk.github.io/docs/books/theory/theory.pdf","joakim-bilyk.github.io/docs/static/theory.pdf")
 
 #Render exercise book
-files <- list.files(path = "_assets/exercises/docs",include.dirs = T, full.names = T)
-file.remove(files)
+unlink("_assets/exercises/docs",recursive = TRUE)
+dir.create("_assets/exercises/docs")
 rmarkdown::render_site("_assets/exercises",encoding = 'ISO8859-1')
 dir.create("joakim-bilyk.github.io/docs/books/exercises")
 files <- list.files(path = "_assets/exercises/docs",include.dirs = T, full.names = T)
 file.copy(files,"joakim-bilyk.github.io/docs/books/exercises",recursive = TRUE)
-file.remove("joakim-bilyk.github.io/docs/pdf/render/exercises.pdf")
-file.copy("joakim-bilyk.github.io/docs/books/exercises/exercises.pdf","joakim-bilyk.github.io/docs/pdf/render/exercises.pdf")
+file.remove("joakim-bilyk.github.io/docs/static/exercises.pdf")
+file.copy("joakim-bilyk.github.io/docs/books/exercises/exercises.pdf","joakim-bilyk.github.io/docs/static/exercises.pdf")
 
 #Render exam prep book
-files <- list.files(path = "_assets/exam/docs",include.dirs = T, full.names = T)
-file.remove(files)
+unlink("_assets/exam/docs",recursive = TRUE)
+dir.create("_assets/exam/docs")
 rmarkdown::render_site("_assets/exam",encoding = 'ISO8859-1')
 dir.create("joakim-bilyk.github.io/docs/books/exam")
 files <- list.files(path = "_assets/exam/docs",include.dirs = T, full.names = T)
 file.copy(files,"joakim-bilyk.github.io/docs/books/exam",recursive = TRUE)
-file.remove("joakim-bilyk.github.io/docs/pdf/render/exam.pdf")
-file.copy("joakim-bilyk.github.io/docs/books/exam/exam.pdf","joakim-bilyk.github.io/docs/pdf/render/exam.pdf")
-
-#Get statics
-unlink("joakim-bilyk.github.io/docs/static",recursive = TRUE)
-dir.create("joakim-bilyk.github.io/docs/static")
-files <- list.files(path = "_assets/static",include.dirs = T, full.names = T)
-file.copy(files,"joakim-bilyk.github.io/docs/static",recursive = TRUE)
+file.remove("joakim-bilyk.github.io/docs/static/exam.pdf")
+file.copy("joakim-bilyk.github.io/docs/books/exam/exam.pdf","joakim-bilyk.github.io/docs/static/exam.pdf")
